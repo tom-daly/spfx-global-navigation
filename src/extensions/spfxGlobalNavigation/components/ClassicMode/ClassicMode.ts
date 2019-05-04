73caad53fb9299a566f3a450f2fea079168913c9
tdaly <tom.m.daly@gmail.com>
2019-05-03 21:23:49 -0400

    updates

--- src/extensions/spfxGlobalNavigation/components/ClassicMode/ClassicMode.ts
+++ src/extensions/spfxGlobalNavigation/components/ClassicMode/ClassicMode.ts
@@ -5,7 +5,7 @@ import GlobalNav from "../GlobalNav/GlobalNav";
 
 require("./classic.scss");
 
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
     let topNavNode = document.getElementById("DeltaTopNavigation");
     ReactDOM.render(
         React.createElement(GlobalNav, {}),
