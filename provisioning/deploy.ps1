909575271f8b34c9b7ea209ad7c0720d5b55664f
tdaly <tom.m.daly@gmail.com>
2019-08-03 11:15:55 -0400

    add lookupfield to deploy script

--- provisioning/deploy.ps1
+++ provisioning/deploy.ps1
@@ -17,4 +17,19 @@ function ProvisionLists() {
     Write-Host "Provisioning done" -ForegroundColor Blue
 }
 
+function ConfigureLookupField() {
+    Write-Host "Adding Lookup Field" -ForegroundColor Green
+    $globalNavList = Get-PnPList -Identity "Lists/GlobalNavList"
+    
+    if(!$globalNavList) {
+        Write-Host "Could not find the list to connect the lookup field. Check that the Global Nav list exists.";
+        return;
+    }
+    else {
+        $fieldXml = "<Field ID='{068992B0-C110-411E-A152-4C17E17E43DE}' Name='GlobalNavParent' StaticName='GlobalNavParent' DisplayName='Parent' Group='Global Nav Site Columns' Required='false' Type='Lookup' List='"+ $globalNavList.Id +"' ShowField='Title' Overwrite='TRUE' OverwriteInChildScopes='TRUE' />"
+        Add-PnPFieldFromXml -List "Global Nav List" -FieldXml $fieldXml | Out-Null
+    }
+}
+
 ProvisionLists
+ConfigureLookupField
