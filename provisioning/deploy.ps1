3105ab7beb35b84edda210ce1e82ef7e64a88d76
Thomas Daly <tom.m.daly@gmail.com>
2019-08-19 09:59:32 -0400

    suppress error if lookup field already exists

--- provisioning/deploy.ps1
+++ provisioning/deploy.ps1
@@ -20,14 +20,14 @@ function ProvisionLists() {
 function ConfigureLookupField() {
     Write-Host "Adding Lookup Field" -ForegroundColor Green
     $globalNavList = Get-PnPList -Identity "Lists/GlobalNavList"
-    
+
     if(!$globalNavList) {
         Write-Host "Could not find the list to connect the lookup field. Check that the Global Nav list exists.";
         return;
     }
     else {
         $fieldXml = "<Field ID='{068992B0-C110-411E-A152-4C17E17E43DE}' Name='GlobalNavParent' StaticName='GlobalNavParent' DisplayName='Parent' Group='Global Nav Site Columns' Required='false' Type='Lookup' List='"+ $globalNavList.Id +"' ShowField='Title' Overwrite='TRUE' OverwriteInChildScopes='TRUE' />"
-        Add-PnPFieldFromXml -List "Global Nav List" -FieldXml $fieldXml | Out-Null
+        Add-PnPFieldFromXml -List "Global Nav List" -FieldXml $fieldXml -ErrorAction "SilentlyContinue"
     }
 }
 
--- provisioning/provisionGlobalNavigation.ps1
+++ provisioning/provisionGlobalNavigation.ps1
@@ -11,7 +11,7 @@ function ProvisionList() {
     Write-Host ""
     Write-Host "Provisioning Site Columns, Content Types, & Lists" -ForegroundColor Yellow
     Write-Host "-------------------------------------------------" -ForegroundColor Yellow
-    
+
     Write-Host "Global Navigation" -ForegroundColor Green
     Apply-PnPProvisioningTemplate ".\GlobalNavList\definition.xml"
 }
@@ -21,18 +21,18 @@ function ProvisionList() {
 function ConfigureLookupField() {
     Write-Host "Adding Lookup Field" -ForegroundColor Green
     $globalNavList = Get-PnPList -Identity "Lists/GlobalNavList"
-    
+
     if(!$globalNavList) {
         Write-Host "Could not find the list to connect the lookup field. Check that the Global Nav list exists.";
         return;
     }
     else {
         $fieldXml = "<Field ID='{068992B0-C110-411E-A152-4C17E17E43DE}' Name='GlobalNavParent' StaticName='GlobalNavParent' DisplayName='Parent' Group='Global Nav Site Columns' Required='false' Type='Lookup' List='"+ $globalNavList.Id +"' ShowField='Title' Overwrite='TRUE' OverwriteInChildScopes='TRUE' />"
-        Add-PnPFieldFromXml -List "Global Nav List" -FieldXml $fieldXml | Out-Null
+        Add-PnPFieldFromXml -List "Global Nav List" -FieldXml $fieldXml -ErrorAction "SilentlyContinue"
     }
 }
 
-# MAIN 
+# MAIN
 Remove-PnPList -Identity "Global Nav List"
 ProvisionList
 ConfigureLookupField
